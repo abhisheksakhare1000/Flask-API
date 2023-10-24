268ab4f27e65d473657df77b17714af995aff4bf
Abhishek Sakhare <96978179+abhisheksakhare1000@users.noreply.github.com>
2023-10-25 00:47:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,54 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def homepage():
-    return "welcome to the home page<h1>"
+    return render_template('index.html') 
 
-@app.route("/add", methods=['POST'])
-def addition():
+
+# API testing
+@app.route("/demo", methods=['POST'])
+def calculate1 ():
     if request.method == "POST":
-        result = int(request.json['num1']) + int(request.json['num2'])
-        return "The summation is {}".format(result)
+        operation = request.json['operation']
+        num1 = int(request.json['num1'])
+        num2 = int(request.json['num2'])
+        result = 0
 
+        if operation == "add":
+            result = num1 + num2
+        elif operation == "subtract":
+            result = num1 - num2
+        elif operation == "multiply":
+            result = num1 * num2
+        elif operation == "divide":
+            result = num1 / num2
+
+        return "The result is {}".format(result)
+        
+        
+
+# getting input through forms
+
+@app.route("/operation", methods=['POST'])
+def calculate():
+    if request.method == "POST":
+        operation = request.form['operation']
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        result = 0
+
+        if operation == "add":
+            result = num1 + num2
+        elif operation == "subtract":
+            result = num1 - num2
+        elif operation == "multiply":
+            result = num1 * num2
+        elif operation == "divide":
+            result = num1 / num2
+
+        return render_template("result.html", result=result)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
